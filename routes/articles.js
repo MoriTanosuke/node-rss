629059c516e70ba5708d0da06148c908b6ec749a
Carsten Ringe <carsten@kopis.de>
2013-05-05 13:41:08 +0200

    Switch from mongodb to mongoosejs

--- routes/articles.js
+++ routes/articles.js
@@ -1,24 +1,19 @@
 
-var db = require('../db');
+var db = require('../db')
+  , mongoose = require('mongoose');
+
+var Article = mongoose.model('Article', articleSchema);
+var Feed = mongoose.model('Feed', feedSchema);
 
 /*
  * GET articles
  */
 exports.list = function(req, res) {
-  db.articles(function(err, collection) {
-    if(err) console.log(err);
     // TODO make sort order configurable
-    collection.find().sort({pubdate: -1}).toArray(function(err, items) {
-      if(err) console.log(err);
-      //TODO check accept header
-      var articles = new Array();
-      for(i in items) {
-        articles.push(items[i]);
-      }
-      res.render('articles', { title: "Articles", user: req.user, articles: articles });
-    });
+  Article.all(function(err, articles) {
+    if(err) console.log(err);
+    res.render('articles', { title: "Articles", user: req.user, articles: articles });
     //TODO send feeds to frontend
     //TODO create nice listing
   });
 };
-
